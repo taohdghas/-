22410deb0bc7b4797ef3c147065cf480f01ad6e7
taohdghas <143993457+taohdghas@users.noreply.github.com>
2023-10-05 13:10:00 +0900

    変数

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <Novice.h>
 
-const char kWindowTitle[] = "";
+const char kWindowTitle[] = "チーム制作";
+
+struct Vector2 {	// ベクトル
+	float x;
+	float y;
+};
+
+//プレイヤー構造体
+struct Box {
+	Vector2 pos;
+	Vector2 velocity;
+	Vector2 direction;
+	float radius;
+	unsigned int color;
+	int isthere;
+	int hp;
+};
+
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};

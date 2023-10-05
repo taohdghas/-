814dbf431e17ea5b27923c2eebc75e3b905353e8
taohdghas <143993457+taohdghas@users.noreply.github.com>
2023-10-05 16:22:45 +0900

    更新

--- main.cpp
+++ main.cpp
@@ -2,43 +2,49 @@
 
 const char kWindowTitle[] = "チーム制作";
 
-struct Vector2 {	// ベクトル
-	float x;
-	float y;
+//ベクトル
+struct Vector2 {
+	float x;                  //X軸
+	float y;                  //Y軸
 };
+
+//サイズ
 struct Size2 {
 	const int width;          //幅
 	const int height;         //高さ
 };
 
-//プレイヤー構造体
+//プレイヤーの構造体
 struct Player {
 	Vector2 pos;              //場所
 	float radius;             //大きさ
 	unsigned int color;       //色
-	int isthere;              //生きているか
-	int hp;                   //HP
+	float angle;              //照準方向
+	int isthere;              //生存フラグ
+	int stock;                //残機
 };
 
+//弾の構造体
 struct Ball {
 	Vector2 pos;              //場所
 	Vector2 velocity;         //速度
 	float radius;             //大きさ
 	unsigned int color;       //色
-	int isshot;               //生きているか
-	int numberofball;         //玉の数
+	int isshot;               //発射フラグ
+	int HP;                   //反射に耐えられる回数
 };
 
+//敵の構造体
 struct Enemy {
 	Vector2 pos;              //場所
 	float radius;             //大きさ
-	int isthere;              //生きているか
+	int isthere;              //生存フラグ
 	int changeblock;          //ブロックの切り替えフラグ
 };
 
+//ステージの構造体
 struct Stage {
-	int number;              //ステージ番号
-	int clear;              //クリアしたか
+	int clear;                //クリアしたか
 
 };
 
@@ -71,55 +77,72 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{0,0},              //場所
 		0,                  //大きさ
 		0xffffffff,         //色
-		true,               //生きているか
+		90.0f,              //照準方向
+		true,               //生存フラグ
+		3                   //残機
 	};
 
 	//玉
 	Ball ball{
 		{player.pos.x,player.pos.y + 10}, //場所
 		{0,0},                            //速度 仮で0
-		10,                               //大きさ 仮
-		0xffffffff,                       //色 仮
-		false,                            //
-		3,
+		10,                               //大きさ 仮で10
+		0xffffffff,                       //色 仮で0
+		false,                            //発射フラグ
+		3,                                //HP 仮で3
 	};
 
 	//敵
 	Enemy enemyUP{
-		{0,0},
-		25,
-		0,
-		0,
+		{0,0},      //場所
+		25,         //大きさ
+		0,          //生存フラグ
+		0,          //ブロックの切り替えフラグ
 	};
 
 	Enemy enemyDOWN{
-		{0,0},
-		25,
-		0,
-		0,
+		{0,0},      //場所
+		25,         //大きさ
+		0,          //生存フラグ
+		0,          //ブロックの切り替えフラグ
 	};
 
 	Enemy enemyLEFT{
-		{0,0},
-		25,
-		0,
-		0,
+		{0,0},      //場所
+		25,         //大きさ
+		0,          //生存フラグ
+		0,          //ブロックの切り替えフラグ
 	};
 
 	Enemy enemyLIGHT{
-		{0,0},
-		25,
-		0,
-		0,
+		{0,0},      //場所
+		25,         //大きさ
+		0,          //生存フラグ
+		0,          //ブロックの切り替えフラグ
+	};
+	
+	Stage stage1{
+		false,
+	};
+
+	Stage stage2{
+		false,
+	};
+
+	Stage stage3{
+	    false,
 	};
 
 	// 画面変化
-	int screenscene = 0;
 	enum screen {
-		start,  //0
-		seen1,  //1
-		clear   //2
+		TITLE,  //タイトル
+		//SEKECT ステージセレクト
+		PLAY,   //ゲーム画面
+		CLEAR   //クリア画面
 	};
+
+	screen screenscene = PLAY; //表示されるシーン
+
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
@@ -136,28 +159,57 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		
-		//玉の発射処理
 
 		switch(screenscene){
-		case start:
+		case TITLE:
+			//spaceキー押されたらセレクト画面に移行(未定)
+			/*if (keys[DIK_SPACE]) {
+			   screenscene = SELECT;
+			};
+			*/
+
+			//spaceキー押されたらステージ画面へ移行
+			if (keys[DIK_SPACE]) {
+				screenscene = PLAY;
+			}
+
 			break;
-		case seen1:
+		//case SELECT:
+			//break;
+		case PLAY:
+
+			//弾が発射できる状態であればプレイヤーを操作できる
+			if (!ball.isshot) {
+				//玉の発射方向
+				if(keys[DIK_LEFTARROW] && !preKeys[DIK_LEFTARROW]){
+				}
+				//玉の発射処理
+				//if (keys[DIK_SPACE] && ball.isshot) {}
+
+			}
+
 			break;
-		case clear:
-		break;
+		case CLEAR:
+			break;
 		}
-		///
-		/// ↑更新処理ここまで
-		///
+		    ///
+		  	/// ↑更新処理ここまで
+			///
 
-		///
-		/// ↓描画処理ここから
-		///
-
-		///
-		/// ↑描画処理ここまで
-		///
+			///
+			/// ↓描画処理ここから
+			///
+		switch (screenscene) {
+		case TITLE:
+			break;
+		case PLAY:
+			break;
+		case CLEAR:
+			break;
+		}
+			///
+			/// ↑描画処理ここまで
+			///
 
 		// フレームの終了
 		Novice::EndFrame();
